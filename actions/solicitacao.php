883ce12a3bf5b1c61b6826bdeeeb82a4e4d5689f
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-14 12:05:09 -0300

    Adiciona action para processamento de solicitações

---
+++ actions/solicitacao.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../lib/configService.php';
+require_once __DIR__ . '/../Connection.php';
+require_once __DIR__ . '/../helpers/SessionHelper.php';
+require_once __DIR__ . '/../lib/solicitacaoService.php';
+
+SessionHelper::requerPerfil('admin');
+SessionHelper::requerMetodoPost();
+
+if (SolicitacaoService::existeSolicitacaoPendente()) {
+    header('Location: ../views/index.php?erro=solicitacao_existente');
+    exit;
+}
+
+try {
+    $resultado = SolicitacaoService::criarSolicitacao(1, 'liberação_estoque');
+    if ($resultado) {
+        header('Location: ../views/index.php?sucesso=solicitacao_enviada');
+    } else {
+        header('Location: ../views/index.php?erro=falha_criacao');
+    }
+
+    exit;
+} catch (PDOException $e) {
+    header('Location: ../views/index.php?erro=erro_banco');
+    exit;
+}
